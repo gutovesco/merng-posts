d4bae24ca41063d7c4731e6b558aef1ea35f7ed7
gutovesco <guto.vesco@hotmail.com>
2020-12-31 11:40:59 -0300

    header in requests and cache to fetch posts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useQuery } from "@apollo/react-hooks";
-import gql from 'graphql-tag';
 import { Grid } from 'semantic-ui-react';
 import PostCard from '../components/PostCard';
+import { AuthContext } from '../context/auth';
+import PostForm from '../components/PostForm';
+import {FETCH_POSTS_QUERY} from '../utils/graphql';
 
 const Home = () => {
+    const { user } = useContext(AuthContext);
     const { loading, data } = useQuery(FETCH_POSTS_QUERY)
 
     return (
@@ -13,9 +16,14 @@ const Home = () => {
                 <>
                     <Grid columns={3}>
                         <Grid.Row>
-                            <h1 style={{display: 'flex', alignSelf: 'center', fontSize: '2rem', marginLeft: 15, marginTop: 20}}>Recent posts</h1>
+                            <h1 style={{ display: 'flex', alignSelf: 'center', fontSize: '2rem', marginLeft: 15, marginTop: 20 }}>Recent posts</h1>
                         </Grid.Row>
                         <Grid.Row>
+                            {user && (
+                                <Grid.Column>
+                                    <PostForm />
+                                </Grid.Column>
+                            )}
                             {loading ? <div>Loading...</div> :
                                 data.getPosts && data.getPosts.map(post => (
                                     <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
@@ -29,24 +37,5 @@ const Home = () => {
         </div>
     )
 }
-
-const FETCH_POSTS_QUERY = gql`
- {
-    getPosts{
-     id 
-     body 
-     createdAt 
-     username 
-     likeCount
-     likes{
-        username
-     }
-     commentCount
-     comments{
-        id username createdAt body
-     }
- }
- }
-`
 
 export default Home;
